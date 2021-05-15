d4eb3f5d1b7e6e2e505f776e64e22ecb459c9ead
rachnasapra <76968050+rachnasapra@users.noreply.github.com>
2021-05-15 12:42:10 +0530

    Add files via upload

--- workspace/app/run.py
+++ workspace/app/run.py
@@ -4,12 +4,22 @@ import pandas as pd
 
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
+from sklearn.pipeline                import Pipeline, FeatureUnion
+from sklearn.multioutput             import MultiOutputClassifier
+from sklearn.ensemble                import RandomForestClassifier, AdaBoostClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, TfidfVectorizer
+from sklearn.model_selection         import train_test_split, GridSearchCV
+from sklearn.metrics                 import make_scorer, accuracy_score, f1_score, fbeta_score, classification_report
+from scipy.stats.mstats              import gmean
+from sklearn.base                    import BaseEstimator, TransformerMixin
 
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
+from plotly.graph_objs import Pie
 from sklearn.externals import joblib
 from sqlalchemy import create_engine
+
 
 
 app = Flask(__name__)
@@ -44,10 +54,32 @@ def index():
     # TODO: Below is an example - modify to extract data for your own visuals
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
+    category_names      = df.iloc[:,4:].columns
+    category_counts     = (df.iloc[:,4:] != 0).sum().values
+    category_percentage = category_counts/category_counts.sum()
     
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
+    #graphs = [
+       # {
+            #'data': [
+              #  Bar(
+                  #  x=genre_names,
+                  #  y=genre_counts
+               # )
+           # ],
+
+            # 'title': 'Distribution of Message Genres',
+               # 'yaxis': {
+                 #   'title': "Count"
+               # },
+               # 'xaxis': {
+                #    'title': "Genre"
+              #  }
+            #}
+       # }
     graphs = [
+        # Graph 1: Genre Graph
         {
             'data': [
                 Bar(
@@ -65,8 +97,43 @@ def index():
                     'title': "Genre"
                 }
             }
+        },
+        
+        # Graph 2: Category Graph
+        {
+            'data': [
+                Pie(
+                    labels = category_names,
+                    values = category_percentage
+                )
+            ],
+
+            'layout': { 'title': 'Percentage of Message Categories' }
+        },
+        
+        # Graph 3: Category Graph
+        {
+            'data': [
+                Bar(
+                    x=category_names,
+                    y=category_counts
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Message Categories',
+                'yaxis': {
+                    'title': "Message Count of each Categories"
+                },
+                'xaxis': {
+                    'title': "Messages Category",
+                    'tickangle': 30
+                }
+            }
         }
+        
     ]
+    
     
     # encode plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
